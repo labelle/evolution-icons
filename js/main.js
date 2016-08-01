6ae91ebd3fc9244aaad61d928b7ccd6f3493ea07
Daniel Tomic <tom_danny2003@yahoo.com>
2016-08-01 11:40:42 -0400

    all active links get class of bold

--- js/main.js
+++ js/main.js
@@ -72,3 +72,18 @@
               // console.log(addClassName)
 
             });
+
+
+
+
+  function highlightCurrentLink() {
+      var arr = document.getElementsByTagName("a");
+      for (var i = 0, len = arr.length; i < len; ++i) {
+          if (arr[i].href == window.location.href) {
+              arr[i].style.fontWeight = "bold";
+              // break;
+          }
+      }
+  }
+  highlightCurrentLink();
+
